7f993953f98f30ae09e06de9cb742f656aa8086e
Amit Gupta <116447781+Amit0200@users.noreply.github.com>
2023-12-26 11:45:45 -0600

    Update bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -1,1 +1,42 @@
+#inlcude <iostream>
+#include <iomanip>
 
+void showBalance(balance){
+
+  std::cout << "Your account balance is: " << std:endl;
+}
+
+void depositMoney(){
+  double money;
+  std::cout << " Enter the amount you would like to deposit: " << std::endl;
+  std::cin >> money;
+
+  std::cout << " Your account balance is: " << money << std::cout;
+}
+
+void withdrawMoney(){
+  double money;
+  
+}
+
+int main(){
+
+  double balane {0};
+  int option {0};
+
+  do{
+    std::cout << " ^ Welcome to ABC banking system ^ " << std::endl << "Choose your option: " << std::endl;
+    std::cout << " 1. Show Balance " << std::endl;
+    std::cout << " 2. Deposit Money " << std::endl;
+    std::cout << " 3. Withdraw Money " << std::endl;
+    std::cout << " 4. Exit " << std::endl;
+
+    std::cin >> option;
+    
+    
+  }
+
+  
+
+  return 0;
+}
