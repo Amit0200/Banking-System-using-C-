78b813a1c9f0c5832a1f8b2b0a7f1275d53c8307
Amit Gupta <116447781+Amit0200@users.noreply.github.com>
2023-12-26 11:51:05 -0600

    Update bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -32,11 +32,26 @@ int main(){
     std::cout << " 4. Exit " << std::endl;
 
     std::cin >> option;
-    
-    
-  }
 
-  
+    std::cin.clear();
+    fflush(stdin);
+
+    switch(option);
+      case 1 : showBalance(balance);
+               break;
+      case 2 : balance += depositMoney();
+               showBalance(balance);
+               break;
+      case 3 : balance -= withdrawMoney();
+               showBalance(balance);
+               break;
+      case 4 : std::cout << " Thanks for visiting our bank!!" << std::endl;
+               break;
+      default : std::cout << " Invalid Option !!! " << std::endl;
+               break;
+  } while {
+    option != 4;
+  }
 
   return 0;
 }
